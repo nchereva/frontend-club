9f6860d7e719f3be20bc15482d11923aaa55d8f4
akamornikov <akamornikov@qulix.com>
2015-04-13 15:08:25 +0300

    Revert "Curry implementation by Kamornikov A."
    This reverts commit 441c4aca252a0db2c463b74a1ee2ac1979949631.

--- curry/index.js
+++ curry/index.js
@@ -8,31 +8,7 @@ suite('Curry', function () {
 
 	setup(function () {
 		// your implementation of curry
-		curry = function(func, arity) {
-			var curriedArgs = [],
-				argsLength,
-				innerCurry = function() {
-					var i,
-						args = [].slice.call(arguments);
-
-					args.forEach(function(cur) {
-						curriedArgs.push(cur);
-					});
-						
-					if (curriedArgs.length < argsLength) {
-						return innerCurry;
-					}
-
-					return func.apply(this, curriedArgs);
-				};
-
-			if (typeof func !== 'function') {
-				throw Error("Argument func is not a function");
-			}
-			argsLength = isNaN(arity) ? func.length : arity;
-
-			return innerCurry;
-		};
+		curry = _.curry;
 	});
 
 	test('test of function currying', function () {

5b7299812103b68873296323c1223be50422eeb6
divizionbyzero <divizionbyzero@bk.ru>
2015-04-16 00:09:54 +0300

    update homework - 2

--- lodash/index.js
+++ lodash/index.js
@@ -23,7 +23,7 @@ suite('Lodash', function() {
         // найдите сумму чисел в массиве
         // с использованием функций _.sum
 
-        var sum = _.sum(arr);
+        var sum = _.sum;
 
         assert.deepEqual(sum([1, 2, 3]), 6);
 
@@ -55,7 +55,7 @@ suite('Lodash', function() {
         var triple = function(el) {
             return el*3;
         };
-        var tripleList = _.map(f,triple);
+        var tripleList = _.map(triple);
 
         assert.deepEqual(tripleList([1, 2, 3], triple), [3, 6, 9]);
     });
@@ -67,7 +67,7 @@ suite('Lodash', function() {
 
         var greater = function(acc, element) {
             return acc > element?acc:element};
-            max = _.reduce(a,greater);
+            max = _.reduce(_,greater);
 
         assert.equal(max([1, -3483, 94, 7, 2]), 94);
         assert.equal(max([-21, -3483, -2, -1]), -1);
@@ -79,7 +79,7 @@ suite('Lodash', function() {
         // * c использованием _.curry
         // ** c использованием _.flow
 
-        var sumNumbers =  _.filter(arr,_.isNumber);
+        var sumNumbers =  _.filter(_,_.isNumber);
 
         assert.equal(sumNumbers([1, '2', 3, '4', 5]), 9);
     });
@@ -88,7 +88,7 @@ suite('Lodash', function() {
         // вберите из массива книг их названия
         // c использованием _.pluck, _.partial
 
-        var getTitles = _.pluck(books,'title');
+        var getTitles = _.pluck(_,'title');
 
         assert.deepEqual(getTitles(articles),['Everything Sucks', 'If You Please'])
 	  });
@@ -105,7 +105,7 @@ suite('Lodash', function() {
             });
             return result?true:false;
         };
-        
+
         assert.isFalse(isAuthor(articles, 'New Guy'));
         assert.isTrue(isAuthor(articles , 'Debbie Downer'));
 					

1346c94fe3ab8899e7c36b113e0e1063ba2ae28d
akamornikov <akamornikov@qulix.com>
2015-04-16 17:13:46 +0300

    Revert "Lodash task by Kamornikov A."
    This reverts commit 0ae74e34f0bba45a93bdc787b34a26b787fd2d2c.

--- lodash/index.js
+++ lodash/index.js
@@ -22,7 +22,7 @@ suite('Lodash', function() {
     test('challenge 1', function() {
         // найдите сумму чисел в массиве
         // с использованием функций _.sum
-        var sum = _.sum;
+        var sum = _.identity;
 
         assert.deepEqual(sum([1, 2, 3]), 6);
     });
@@ -30,7 +30,7 @@ suite('Lodash', function() {
     test('challenge 1.1', function() {
         // найдите сумму уникальных чисел в массиве
         // с использованием функций _.sum, _.flow, _.uniq
-        var sum = _.flow(_.uniq, _.sum);
+        var sum = _.identity;
 
         assert.deepEqual(sum([1, 2, 1 , 3, 3, 2]), 6);
     });
@@ -38,7 +38,7 @@ suite('Lodash', function() {
     test('challenge 1.2', function() {
         // найдите сумму уникальных чисел их всех массивах
         // с использованием функций _.sum, _.flow, _.uniq, _.union
-        var sum = _.flow(_.union, _.uniq, _.sum);
+        var sum = _.identity;
 
         assert.deepEqual(sum([1, 2, 3], [1], [1, 2]), 6);
     });
@@ -46,8 +46,8 @@ suite('Lodash', function() {
     test('challenge 2', function() {
         // напишите функцию которая умножает каждый элемент массива на 3
         // с использованием функций _.map
-        var triple = function(num) { return num * 3}, 
-            tripleList = _.map;
+        var triple = _.identity, 
+            tripleList = _.identity;
 
         assert.deepEqual(tripleList([1, 2, 3], triple), [3, 6, 9]);
     });
@@ -56,8 +56,8 @@ suite('Lodash', function() {
         // напишите функцию для нахождения самого большого числа в массиве
         // с использованием функций _.reduce
         // * c использованием _.partialRight
-        var greater = function(great, cur) { return great < cur ? cur : great; },
-            max = _.partialRight(_.reduce, greater);
+        var greater = _.identity,
+            max = _.identity;
 
         assert.equal(max([1, -3483, 94, 7, 2]), 94);
         assert.equal(max([-21, -3483, -2, -1]), -1);
@@ -68,8 +68,7 @@ suite('Lodash', function() {
         // с использованием функций _.filter,_.isNumber, ...
         // * c использованием _.curry
         // ** c использованием _.flow
-        var filter = _.curry(_.filter)(_, _.isNumber, this), 
-            sumNumbers = _.flow(filter, _.sum);
+        var sumNumbers = _.identity;
 
         assert.equal(sumNumbers([1, '2', 3, '4', 5]), 9);
     });
@@ -77,7 +76,7 @@ suite('Lodash', function() {
     test('Challenge 5', function(){
         // вберите из массива книг их названия
         // c использованием _.pluck, _.partial
-        var getTitles = _.partial(_.pluck, _, "title");
+        var getTitles = _.identity;
 
         assert.deepEqual(getTitles(articles),['Everything Sucks', 'If You Please'])
     });
@@ -85,9 +84,7 @@ suite('Lodash', function() {
     test('Challenge 6', function(){
         //напишите функцию наличия в массиве обьектов запрашиваемого автора
         // c использованием _.find, ...
-        var isAuthor = function(collection, name) { 
-            return (_.find(collection, function(cur) { return cur.author.name === name; } )) ? true : false;
-        };
+        var isAuthor = _.identity;
 
         assert.isFalse(isAuthor(articles, 'New Guy'));
         assert.isTrue(isAuthor(articles , 'Debbie Downer'));
@@ -95,29 +92,12 @@ suite('Lodash', function() {
 
     test('Challenge 7✯✯✯', function(){
         var a = [1],
-            b = [1],
-            newPush,
-            maxPushCount = 2;
+            b = [1];
 
         // your code goes here
         // как видно из примера в массив можно занести лишь первые 2 значения 
         // остальные игнорируются
         // на этот раз без подсказок
-
-        newPush = function(elem) {
-            var counter = 0;
-
-            return function(elem) {
-                if (counter < maxPushCount) {
-                    counter++;
-                    Array.prototype.push.call(this, elem);
-                }
-            };
-        }();
-
-        Object.defineProperty(a, "push", {
-            value : newPush
-        });
 
         a.push(2);
         a.push(3);
